e2fec86c1fe0f441a04b6221822a915e536d7c4f
mecodeur <mehdi.el2023@icloud.com>
2023-09-01 20:30:19 +0100

    [Feature] set the user data after the registration

--- lib/controllers/database_controller.dart
+++ lib/controllers/database_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_ecommerce/models/product.dart';
+import 'package:flutter_ecommerce/models/user_data.dart';
 import 'package:flutter_ecommerce/services/firestore_services.dart';
 
 import '../utilities/api_path.dart';
@@ -8,7 +9,7 @@ abstract class Database {
 
   Stream<List<Product>> salesProductsStream();
 
-  /*Future<void> setData();*/
+  Future<void> setUserData(UserData userData);
 }
 
 class FirestoreDatabase implements Database {
@@ -31,12 +32,12 @@ class FirestoreDatabase implements Database {
         builder: (data, documentId) => Product.fromMap(data!, documentId),
       );
 
-  /*
   @override
-  Future<void> setProduct(Product product) async {
-    return _service.setData(
-        path: ApiPath.products(),
-        data: product.toMap());
-  }*/
+  Future<void> setUserData(UserData userData) async{
+    return await _service.setData(
+      path: ApiPath.user(userData.uid),
+      data: userData.toMap(),
+    );
+  }
 
 }
